158624502a87743a9745f1fc3834f13bc0927f6f
muhammadsodiq1429 <muhammadsodiqmuhammadjanov@gmail.com>
2025-08-23 07:04:21 +0500

    unused files deleted

--- src/common/enums/user-gender.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserGender = void 0;
-var UserGender;
-(function (UserGender) {
-    UserGender["MALE"] = "male";
-    UserGender["FEMALE"] = "female";
-    UserGender["PREFER_NOT_TO_SAY"] = "prefer_not_to_say";
-})(UserGender || (exports.UserGender = UserGender = {}));
--- src/common/helpers/addMinutes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addMinutesToDate = addMinutesToDate;
-function addMinutesToDate(date, minutes) {
-    return new Date(date.getTime() + minutes * 60 * 1000);
-}
